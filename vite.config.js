5c9a50289b575b0b9a67b0929fc1c5acca6d3864
sumit singh <sumit121singh@gmail.com>
2025-05-15 13:37:16 +0530

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // or './' if you’re serving from a subdirectory
   plugins: [react()],
 })
